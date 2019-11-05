7233ce28ece29dc47594d225039fa646c091f701
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-11-05 20:29:11 +0000

    if/else and while

--- Module_1.py
+++ Module_1.py
@@ -1,7 +1,20 @@
+# while loop
 
-# Flow Control
+spam = 0
+while spam<5:
+    print('There are ' + str(spam) + ' spam')
+    spam = spam + 1
 
 
+# if else
+print('enter name')
+name = input()
+if name == 'Ronan':
+    print('Hi ' + name + 'Finally you have a great name')
+    print('done')
+else:
+    print(name + ' is a dumb name your name shoudl be Ronan')
+    name = input()
 
 
 # This program says hello and asks for my name
